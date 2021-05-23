47ce51b1362791938f68be49e0fbcb37e4184054
Sergey Klimuk <sergey_klimuk@mail.ru>
2021-05-23 18:48:27 +0300

    fix: fix MainActivity

--- app/src/main/java/com/rsschool/android2021/MainActivity.java
+++ app/src/main/java/com/rsschool/android2021/MainActivity.java
@@ -9,7 +9,6 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity implements MyFragmentListener {
 
-    // My code
     private final String FIRST_FRAGMENT_TAG = "firstFragment";
     private final String SECOND_FRAGMENT_TAG = "secondFragment";
 
@@ -21,28 +20,19 @@ public class MainActivity extends AppCompatActivity implements MyFragmentListene
     }
 
     private void openFirstFragment(int previousNumber) {
-        getSupportFragmentManager().popBackStack();
         final Fragment firstFragment = FirstFragment.newInstance(previousNumber);
         final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.container, firstFragment, FIRST_FRAGMENT_TAG);
-
-        // _TODO: invoke function which apply changes of the transaction
-        // My code
-
         transaction.commit();
     }
 
     private void openSecondFragment(int min, int max) {
-        // _TODO: implement it
-        // My code
         final Fragment secondFragment = SecondFragment.newInstance(min, max);
         final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.container, secondFragment, SECOND_FRAGMENT_TAG);
-        transaction.addToBackStack(null);
         transaction.commit();
     }
 
-    // My code
     @Override
     public void doSomething(String fragmentTag, int... vars) {
         switch (fragmentTag) {
